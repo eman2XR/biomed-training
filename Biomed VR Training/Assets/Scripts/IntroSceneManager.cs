be48e73eda56e6863f7a062595baec537a9ecaa8
eman2XR <emanuel.t.tomozei@gmail.com>
2021-09-16 17:23:06 +0100

    full run-through

--- Biomed VR Training/Assets/Scripts/IntroSceneManager.cs
+++ Biomed VR Training/Assets/Scripts/IntroSceneManager.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class IntroSceneManager : MonoBehaviour
 {
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(4);
+        ChangeScene();
+    }
+
     public void ChangeScene()
     {
-
+        SceneManager.LoadScene("MAIN");
     }
 }
